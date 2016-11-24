ab6b0dd4bd4bbfc1f456fb49baa9ef60007e6c09
Vladyslav <vlad@chill>
2016-11-24 11:48:38 +0200

    Allure dependencies are added

--- src/test/java/com/qatestlab/pages/LoginPage.java
+++ src/test/java/com/qatestlab/pages/LoginPage.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import ru.yandex.qatools.allure.annotations.Step;
+
 import static com.qatestlab.utils.ReporterCustom.log;
 
 public class LoginPage {
@@ -27,23 +29,26 @@ public class LoginPage {
     private WebElement accountName;
 
 
+
     public LoginPage(WebDriver driver) {
         this.driver = driver;
         driver.get(BASE_URL);
         PageFactory.initElements(driver, this);
     }
 
-
+    @Step("Set username {0}")
     public void setUserName(String username){
         loginField.sendKeys(username);
         log("Set username "+username);
     }
 
+    @Step("Set password {0}")
     public void setPassword(String password){
         passwordField.sendKeys(password);
         log("Set password "+password);
     }
 
+    @Step("Click sing in button")
     public void clicksingInButton(){
         singInButton.click();
         log("click sing in button");
@@ -55,6 +60,7 @@ public class LoginPage {
         clicksingInButton();
     }
 
+    @Step("Click account info button")
     public void clickAccountInfoButton(){
         accountInfoButton.click();
         log("click account info button");
